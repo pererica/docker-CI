e1bf98e20acebcf4e9ae6bf6945e7e88b5719ef3
pererica <pereric.andree@gmail.com>
2018-12-02 09:33:32 +0100

    Changed test case

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,14 @@
-//file : test.js
-import { equal } from 'assert';
 
-it('should return true', () => {
-  assert.equal(true, true);
+// Simple async test for HTTP 200 response code using supertest
+'use strict';
+
+var request = require("supertest"),
+    app = require("../app").getApp;
+
+describe('GET /', function(){
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
 });
